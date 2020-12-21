b772c91a9a2ef3c25595b28be699df2fbd694c8f
griffinjoshs <leveloutusa@gmail.com>
2020-12-21 00:29:42 -0500

    server and everything changed

---
+++ Public/assets/js/scripts.js
@@ -0,0 +1,27 @@
+//nav bar background-color change on scroll 
+
+$(window).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
+});
+
+//hamburger color change on click
+$(document).ready(function () {
+    $('.navbar-dark .navbar-toggler').click(function () {
+        $(this).toggleClass('active');
+    });
+});
+
+//change color of navbar when hamburger is clicked
+$(document).ready(function () {
+    $('.navbar-dark .navbar').click(function () {
+        $(this).css("background-color", "black");
+        $(this).toggleClass('active');
+    });
+});
+
+$(".myBox").click(function() {
+    window.location = $(this).find("a").attr("href"); 
+    return false;
+  });
+
+  
